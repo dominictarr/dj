2615d38e2f3c6072a01ead14644a687a23956a86
Dominic Tarr <dominic.tarr@gmail.com>
2013-02-20 02:25:49 +1300

    update build script

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+{
+  echo '<!DOCTYPE HTML><html><body></body><script>'
+  browserify client.js --debug --exports require || exit 1
+  echo '</script></html>'
+} > static/editor.html
+
