3d138552d4054f04a8d03fef279cfaf82cdc51d9
Dominic Tarr <dominic.tarr@gmail.com>
2013-02-22 01:48:19 +1300

    update build

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 {
-  echo '<!DOCTYPE HTML><html><body></body>'
-  echo '<link rel=stylesheet href=./style>'
+  echo '<!DOCTYPE HTML><html><head>'
+  echo '<link rel=stylesheet href=./style.css >'
   echo '<script'\
   'src=http://rumoursdb.com/examples/css-colab/css-colab.js>'\
   '></script>'
   echo '<script>CssColab("DJ")</script>'
-  echo '<script>'
+  echo '</head><body></body><script>'
   browserify client.js --debug --exports require || exit 1
   echo '</script></html>'
 } > static/index.html
