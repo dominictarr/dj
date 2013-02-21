173a7452d1d717b1f9b747af753259ef87469471
Dominic Tarr <dominic.tarr@gmail.com>
2013-02-22 01:28:00 +1300

    add css-colab for style

--- build.sh
+++ build.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
 {
-  echo '<!DOCTYPE HTML><html><body></body><script>'
+  echo '<!DOCTYPE HTML><html><body></body>'
+  echo '<link rel=stylesheet href=./style>'
+  echo '<script'\
+  'src=http://rumoursdb.com/examples/css-colab/css-colab.js>'\
+  '></script>'
+  echo '<script>CssColab("DJ")</script>'
+  echo '<script>'
   browserify client.js --debug --exports require || exit 1
   echo '</script></html>'
 } > static/index.html
