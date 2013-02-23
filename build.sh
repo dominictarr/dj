ee4840f3903f5ceaf97e23ac3640247ca12d3ea1
Dominic Tarr <dominic.tarr@gmail.com>
2013-02-23 20:27:53 +1300

    style is ready

--- build.sh
+++ build.sh
@@ -3,10 +3,10 @@
 {
   echo '<!DOCTYPE HTML><html><head>'
   echo '<link rel=stylesheet href=./style.css >'
-  echo '<script'\
-  'src=http://rumoursdb.com/examples/css-colab/css-colab.js>'\
-  '></script>'
-  echo '<script>CssColab("DJ")</script>'
+#  echo '<script'\
+#  'src=http://rumoursdb.com/examples/css-colab/css-colab.js>'\
+#  '></script>'
+#  echo '<script>CssColab("DJ")</script>'
   echo '</head><body></body><script>'
   browserify client.js --debug --exports require || exit 1
   echo '</script></html>'
